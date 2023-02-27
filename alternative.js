738143af1c7c3ed3800cb84b8624a613193d34dc
Riccardo Avanzi <aidenticapp@gmail.com>
2023-02-27 23:38:52 +0100

    - ADDED: margins, paddings highlight - ADDED: nearest parent highlight

--- alternative.js
+++ alternative.js
@@ -1,18 +1,23 @@
 
+// PUPPETEER
 const fs = require('fs'); // NODE ACCESS TO FILE SYSTEM
 const puppeteer = require('puppeteer'); // PPTR INIT
 
+// BROWSER INITIALISATION
 async function screenshotWithElements(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  
+  // URLS
+  await page.goto('https://www.tencent.com/en-us/');
   await page.setViewport({
       width: 1440,
       height: 1024,
       deviceScaleFactor: 1,
   });
 
-  const elements = await page.$$('button, h1, h2, h3, h4, h5, h6, header, footer, input, form, article, nav, i, svg, video, a, p');
+  // MAPPING ELEMENTS
+  const elements = await page.$$('header, nav, menu, h1, h2, h3, h4, h5, h6, p, a, span, button, ul, li, form, input, img, picture, i, video, svg');
   const elementData = {};
 
   for (let i = 0; i < elements.length; i++) {
@@ -24,13 +29,13 @@ async function screenshotWithElements(url) {
     elementData[tagName] = elementData[tagName] || [];
     elementData[tagName].push({ innerHTML });
 
-    await element.evaluate((element) => {
+    await elements[i].evaluate((element) => {
       element.style.border = '2px solid red';
       const label = document.createElement('div');
       label.textContent = element.tagName.toLowerCase();
       label.style.position = 'absolute';
       label.style.top = 0;
-      label.style.left = 0;
+      label.style.left = 0; 
       label.style.backgroundColor = 'red';
       label.style.color = 'white';
       label.style.padding = '2px';
@@ -40,11 +45,36 @@ async function screenshotWithElements(url) {
   }
 
   await page.screenshot({ path: './screenshots/screenshot.png', fullPage: true});
+  
+  fs.writeFile('./json/elementData.json', JSON.stringify(elementData, null, 2), (err) => {
+    if (err) throw err;
+    console.log('File saved');
+  });
+
   await browser.close();
-
-  const newPage = await browser.newPage();
-  await newPage.goto(`file://${process.cwd()}/screenshot.png`);
-
-  const json = JSON.stringify(elementData);
-  fs.writeFileSync('./screenshots/elementData.json', json);
 }
+
+screenshotWithElements();
+
+
+// const paddingHighlight = document.createElement('div');
+//       paddingHighlight.style.position = 'absolute';
+//       paddingHighlight.style.top = '0';
+//       paddingHighlight.style.left = '0';
+//       paddingHighlight.style.right = '0';
+//       paddingHighlight.style.bottom = '0';
+//       paddingHighlight.style.backgroundColor = 'rgba(191, 79, 255, 0.02)';
+//       paddingHighlight.style.zIndex = '99999';
+//       paddingHighlight.style.padding = padding;
+//       element.appendChild(paddingHighlight);
+
+//       const marginHighlight = document.createElement('div');
+//       marginHighlight.style.position = 'absolute';
+//       marginHighlight.style.top = '-10px';
+//       marginHighlight.style.left = '-10px';
+//       marginHighlight.style.right = '-10px';
+//       marginHighlight.style.bottom = '-10px';
+//       marginHighlight.style.backgroundColor = 'rgba(0, 191, 255, 0.02)';
+//       marginHighlight.style.zIndex = '99999';
+//       marginHighlight.style.margin = margin;
+//       element.insertBefore(marginHighlight, element.firstChild);
--- main.js
+++ main.js
@@ -1,48 +1,21 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
 
-// FIREBASE INIT
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-const firebaseConfig = {
-  apiKey: "AIzaSyDkcXLdP-nXiSf2b0tjzQJnhhgfpKuL_GE",
-  authDomain: "sherlock-79215.firebaseapp.com",
-  projectId: "sherlock-79215",
-  storageBucket: "sherlock-79215.appspot.com",
-  messagingSenderId: "252442690424",
-  appId: "1:252442690424:web:98e1822b6c56ad4f363e8d",
-  measurementId: "G-KVE66S9HX8"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-/////////////////////////////////////
-/////////////////////////////////////
-
-// PUPPETEER
-const fs = require('fs'); // NODE ACCESS TO FILE SYSTEM
-const puppeteer = require('puppeteer'); // PPTR INIT
-
-// BROWSER INITIALISATION
 async function screenshotWithElements(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   
-  // URLS
-  await page.goto('https://www.docker.com');
+  await page.goto(url);
   await page.setViewport({
-      width: 1440,
-      height: 1024,
-      deviceScaleFactor: 1,
+    width: 1440,
+    height: 1024,
+    deviceScaleFactor: 1,
   });
 
-  // MAPPING ELEMENTS
-  const elements = await page.$$('header, nav, menu, h1, h2, h3, h4, h5, h6, strong, p, a, span, button, ul, li, form, input, img, picture, i, video, svg');
+  const elements = await page.$$('header, nav, menu, h1, h2, h3, h4, h5, h6, p, a, span, button, ul, li, form, input, img, picture, i, video, svg');
   const elementData = {};
 
   for (let i = 0; i < elements.length; i++) {
-
     const element = elements[i];
     const tagName = await element.getProperty('tagName').then(tag => tag.jsonValue()).then(tag => tag.toLowerCase());
     const innerHTML = await element.getProperty('innerHTML').then(html => html.jsonValue());
@@ -51,6 +24,7 @@ async function screenshotWithElements(url) {
     elementData[tagName].push({ innerHTML });
 
     await elements[i].evaluate((element) => {
+
       element.style.border = '2px solid red';
       const label = document.createElement('div');
       label.textContent = element.tagName.toLowerCase();
@@ -62,6 +36,41 @@ async function screenshotWithElements(url) {
       label.style.padding = '2px';
       label.style.fontSize = '12px';
       element.appendChild(label);
+
+      const boxModel = window.getComputedStyle(element, null);
+      const padding = boxModel.padding.split(' ').join('px, ') + 'px';
+      const margin = boxModel.margin.split(' ').join('px, ') + 'px';
+      
+      const paddingHighlight = document.createElement('div');
+      paddingHighlight.style.position = 'absolute';
+      paddingHighlight.style.top = '50%';
+      paddingHighlight.style.left = '50%';
+      paddingHighlight.style.transform = 'translate(-50%, -50%)';
+      paddingHighlight.style.boxSizing = 'border-box';
+      paddingHighlight.style.border = '1px dashed #9D96EB';
+      paddingHighlight.style.opacity = '0.5';
+      paddingHighlight.style.width = `calc(100% - ${padding})`;
+      paddingHighlight.style.height = `calc(100% - ${padding})`;
+      paddingHighlight.style.zIndex = '9999';
+      element.appendChild(paddingHighlight);
+
+      const marginHighlight = document.createElement('div');
+      marginHighlight.style.position = 'absolute';
+      marginHighlight.style.top = '50%';
+      marginHighlight.style.left = '50%';
+      marginHighlight.style.transform = 'translate(-50%, -50%)';
+      marginHighlight.style.boxSizing = 'border-box';
+      marginHighlight.style.border = '1px dashed #96DBEB';
+      marginHighlight.style.opacity = '0.5';
+      marginHighlight.style.width = `calc(100% + ${margin})`;
+      marginHighlight.style.height = `calc(100% + ${margin})`;
+      marginHighlight.style.zIndex = '9999';
+      element.insertBefore(marginHighlight, element.firstChild);
+      
+      const parent = element.parentElement;
+      if (parent.tagName.toLowerCase() !== 'body') {
+        parent.style.border = '2px solid #96EBD1';
+      }
     });
   }
 
@@ -73,10 +82,6 @@ async function screenshotWithElements(url) {
   });
 
   await browser.close();
-
-//   const newPage = await browser.newPage();
-//   await newPage.goto(`./screenshots/screenshot.png`);
-
 }
 
-screenshotWithElements();
+screenshotWithElements('https://www.docker.com/');
